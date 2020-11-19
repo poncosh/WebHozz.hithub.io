d6fd814949101f2d8cd0f49312a3b335404f6f8e
poncosh <vhonchosatrio@gmail.com>
2020-11-19 22:06:00 +0700

    Change type

--- headerfooter/header.php
+++ headerfooter/header.php
@@ -8,10 +8,10 @@
    			</div>
    			    <div class="col-sm-6" style="margin-left: 0px;">
    				  <div class="kanan">
-   					 <li><a href="index.html">Home</a></li>
-   					 <li><a href="blog.html">Blog</a></li>
-   					 <li><a href="about.html">About Us</a></li>
-   					 <li><a href="contact.html">Contact</a></li>
+   					 <li><a href="index.php">Home</a></li>
+   					 <li><a href="blog.php">Blog</a></li>
+   					 <li><a href="about.php">About Us</a></li>
+   					 <li><a href="contact.php">Contact</a></li>
    				  </div>
    			    </div>
    	    </div>
